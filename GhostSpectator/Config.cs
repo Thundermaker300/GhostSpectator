416cd0686d4ce0955006e4baf3ccbaa2b5543001
Aer <63594377+AerGameChannel@users.noreply.github.com>
2020-11-14 03:17:19 +0200

    Added EnterFemurBreaker config variable

--- GhostSpectator/Config.cs
+++ GhostSpectator/Config.cs
@@ -46,6 +46,7 @@ namespace GhostSpectator
         public bool InteractScp914 { get; set; } = false;
         public bool InteractIntercom { get; set; } = false;
         public bool Contain106 { get; set; } = false;
+        public bool EnterFemurBreaker { get; set; } = false;
 
         [Description("Sets the string for roles in place of {class} in the above strings (for example, replacing Class-D Personnel with DBOI will make it say DBOI in game.")]
         public Dictionary<RoleType, string> RoleStrings { get; set; } = new Dictionary<RoleType, string>
