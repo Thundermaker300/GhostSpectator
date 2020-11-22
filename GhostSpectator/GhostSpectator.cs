74e9aae16bbf0a707488482178d9c5531021d7fe
Thunder <Thundermaker300@gmail.com>
2020-11-22 17:01:00 -0500

    Adding required exiled version

--- GhostSpectator/GhostSpectator.cs
+++ GhostSpectator/GhostSpectator.cs
@@ -114,6 +114,7 @@ namespace GhostSpectator
         public override string Name => "GhostSpectator";
         public override string Author => "Thunder";
         public override Version Version => new Version(1, 0, 5);
+        public override Version RequiredExiledVersion => new Version(2, 1, 18);
         public override PluginPriority Priority => PluginPriority.High;
     }
 }
