77a03a5bf9180f77b85e59d902bdc18c2014843c
Thundermaker300 <thundermaker300@gmail.com>
2020-12-24 21:25:19 -0500

    Changing minor things

--- GhostSpectator/Patches/SpawnPatch.cs
+++ GhostSpectator/Patches/SpawnPatch.cs
@@ -22,10 +22,7 @@ namespace GhostSpectator.Patches
 				ServerConsole.AddLog("Fatal error. Team '" + __instance.NextKnownTeam + "' is undefined.", ConsoleColor.Red);
 				return false;
 			}
-			List<ReferenceHub> list = (from item in ReferenceHub.GetAllHubs().Values
-									   where (item.characterClassManager.CurClass == global::RoleType.Spectator && !item.serverRoles.OverwatchEnabled)
-									   || API.IsGhost(Player.Get(item))
-									   select item).ToList<ReferenceHub>();
+			List<ReferenceHub> list = Player.List.Where(Ply => (Ply.IsDead && !Ply.IsOverwatchEnabled) || API.IsGhost(Ply)).Select(Ply => Ply.ReferenceHub).ToList();
 			if (__instance._prioritySpawn)
 			{
 				list = (from item in list
@@ -34,7 +31,7 @@ namespace GhostSpectator.Patches
 			}
 			else
 			{
-				list.ShuffleList<ReferenceHub>();
+				list.ShuffleList();
 			}
 			int num = RespawnTickets.Singleton.GetAvailableTickets(__instance.NextKnownTeam);
 			if (num == 0)
@@ -47,63 +44,63 @@ namespace GhostSpectator.Patches
 			{
 				list.RemoveAt(list.Count - 1);
 			}
-			list.ShuffleList<global::ReferenceHub>();
-			List<global::ReferenceHub> list2 = ListPool<global::ReferenceHub>.Shared.Rent();
-			foreach (global::ReferenceHub referenceHub in list)
+			list.ShuffleList();
+			List<ReferenceHub> list2 = ListPool<ReferenceHub>.Shared.Rent();
+			foreach (ReferenceHub referenceHub in list)
 			{
 				try
 				{
-					global::RoleType classid = spawnableTeam.ClassQueue[Mathf.Min(list2.Count, spawnableTeam.ClassQueue.Length - 1)];
+					RoleType classid = spawnableTeam.ClassQueue[Mathf.Min(list2.Count, spawnableTeam.ClassQueue.Length - 1)];
 					referenceHub.characterClassManager.SetPlayersClass(classid, referenceHub.gameObject, false, false);
 					list2.Add(referenceHub);
-					global::ServerLogs.AddLog(global::ServerLogs.Modules.ClassChange, string.Concat(new string[]
+					ServerLogs.AddLog(ServerLogs.Modules.ClassChange, string.Concat(new string[]
 					{
 						"Player ",
 						referenceHub.LoggedNameFromRefHub(),
 						" respawned as ",
 						classid.ToString(),
 						"."
-					}), global::ServerLogs.ServerLogType.GameEvent, false);
+					}), ServerLogs.ServerLogType.GameEvent, false);
 				}
 				catch (Exception ex)
 				{
 					if (referenceHub != null)
 					{
-						global::ServerLogs.AddLog(global::ServerLogs.Modules.ClassChange, "Player " + referenceHub.LoggedNameFromRefHub() + " couldn't be spawned. Err msg: " + ex.Message, global::ServerLogs.ServerLogType.GameEvent, false);
+						ServerLogs.AddLog(ServerLogs.Modules.ClassChange, "Player " + referenceHub.LoggedNameFromRefHub() + " couldn't be spawned. Err msg: " + ex.Message, ServerLogs.ServerLogType.GameEvent, false);
 					}
 					else
 					{
-						global::ServerLogs.AddLog(global::ServerLogs.Modules.ClassChange, "Couldn't spawn a player - target's ReferenceHub is null.", global::ServerLogs.ServerLogType.GameEvent, false);
+						ServerLogs.AddLog(ServerLogs.Modules.ClassChange, "Couldn't spawn a player - target's ReferenceHub is null.", ServerLogs.ServerLogType.GameEvent, false);
 					}
 				}
 			}
 			if (list2.Count > 0)
 			{
-				global::ServerLogs.AddLog(global::ServerLogs.Modules.ClassChange, string.Concat(new object[]
+				ServerLogs.AddLog(ServerLogs.Modules.ClassChange, string.Concat(new object[]
 				{
 					"RespawnManager has successfully spawned ",
 					list2.Count,
 					" players as ",
 					__instance.NextKnownTeam.ToString(),
 					"!"
-				}), global::ServerLogs.ServerLogType.GameEvent, false);
+				}), ServerLogs.ServerLogType.GameEvent, false);
 				RespawnTickets.Singleton.GrantTickets(__instance.NextKnownTeam, -list2.Count * spawnableTeam.TicketRespawnCost, false);
 				Respawning.NamingRules.UnitNamingRule unitNamingRule;
 				if (Respawning.NamingRules.UnitNamingRules.TryGetNamingRule(__instance.NextKnownTeam, out unitNamingRule))
 				{
 					string text;
 					unitNamingRule.GenerateNew(__instance.NextKnownTeam, out text);
-					foreach (global::ReferenceHub referenceHub2 in list2)
+					foreach (ReferenceHub referenceHub2 in list2)
 					{
 						referenceHub2.characterClassManager.NetworkCurSpawnableTeamType = (byte)__instance.NextKnownTeam;
 						referenceHub2.characterClassManager.NetworkCurUnitName = text;
 					}
-					global::SCP_2536_Controller.singleton.TotalSpawnedPlayersByUnitName.Add(text, list2.Count);
+					SCP_2536_Controller.singleton.TotalSpawnedPlayersByUnitName.Add(text, list2.Count);
 					unitNamingRule.PlayEntranceAnnouncement(text);
 				}
 				RespawnEffectsController.ExecuteAllEffects(RespawnEffectsController.EffectType.UponRespawn, __instance.NextKnownTeam);
 			}
-			ListPool<global::ReferenceHub>.Shared.Return(list2);
+			ListPool<ReferenceHub>.Shared.Return(list2);
 			__instance.NextKnownTeam = SpawnableTeamType.None;
 
 			return false;
