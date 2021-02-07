fba6531614faec0514ae8e628d075f7a4c2f5fa6
Thunder <thundermaker300@gmail.com>
2021-02-07 03:02:52 -0500

    Version bump

--- GhostSpectator/GhostSpectator.cs
+++ GhostSpectator/GhostSpectator.cs
@@ -141,7 +141,7 @@ namespace GhostSpectator
 
         public override string Name => "GhostSpectator";
         public override string Author => "Thunder";
-        public override Version Version => new Version(1, 1, 5);
+        public override Version Version => new Version(1, 1, 6);
         public override Version RequiredExiledVersion => new Version(2, 1, 30);
         public override PluginPriority Priority => PluginPriority.High;
     }
