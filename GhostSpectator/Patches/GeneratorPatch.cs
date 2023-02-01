af206424b95745827da87b367206404242507675
Thunder <thundermaker300@gmail.com>
2023-02-01 17:22:09 -0500

    Generator patch

---
+++ GhostSpectator/Patches/GeneratorPatch.cs
@@ -0,0 +1,21 @@
+﻿using Exiled.API.Features;
+using HarmonyLib;
+using MapGeneration.Distributors;
+using PlayerRoles.Voice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostSpectator.Patches
+{
+    [HarmonyPatch(typeof(Scp079Generator), nameof(Scp079Generator.ServerInteract))]
+    public class GeneratorPatch
+    {
+        public static bool Prefix(Scp079Generator __instance, ReferenceHub ply, byte colliderId)
+        {
+            return !API.IsGhost(ply);
+        }
+    }
+}
