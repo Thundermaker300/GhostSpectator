585e58a513f3e10ee804c4bb0bfc51d9b7e37174
Thunder <thundermaker300@gmail.com>
2023-02-01 10:53:26 -0500

    Maybe fix door patch?

--- GhostSpectator/Patches/DoorPatch.cs
+++ GhostSpectator/Patches/DoorPatch.cs
@@ -5,14 +5,17 @@ using Interactables.Interobjects.DoorUtils;
 namespace GhostSpectator.Patches
 {
     // Required in order for doors to not make denied sound.
-    [HarmonyPatch(typeof(DoorVariant), nameof(DoorVariant.ServerInteract))]
+    [HarmonyPatch(typeof(DoorVariant), nameof(DoorVariant.AllowInteracting))]
     public class DoorPatch
     {
-        public static bool Prefix(DoorVariant __instance, ReferenceHub ply, byte colliderId)
+        public static bool Prefix(DoorVariant __instance, ReferenceHub ply, byte colliderId, ref bool __result)
         {
             Player player = Player.Get(ply);
             if (player is not null && API.IsGhost(player))
+            {
+                __result = false;
                 return false;
+            }
 
             return true;
         }
