02c3929401ef9538df93e552029b91850d3c5a7e
Thunder <thundermaker300@gmail.com>
2023-02-01 16:29:49 -0500

    Intercom patch

---
+++ GhostSpectator/Patches/IntercomPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using PlayerRoles.Voice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostSpectator.Patches
+{
+    [HarmonyPatch(typeof(Intercom), nameof(Intercom.CheckRange))]
+    public class IntercomPatch
+    {
+        public static bool Prefix(Intercom __instance, ReferenceHub hub)
+        {
+            return !API.IsGhost(hub);
+        }
+    }
+}
